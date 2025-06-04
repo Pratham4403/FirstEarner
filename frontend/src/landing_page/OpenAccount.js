92b68c086f11bf1c08df3290944fab20c8abe124
Pratham4403 <mohiteprathamesh2005@gmail.com>
2025-06-04 18:37:37 +0530

    Update OpenAccount.js

--- frontend/src/landing_page/OpenAccount.js
+++ frontend/src/landing_page/OpenAccount.js
@@ -7,7 +7,7 @@ export default function OpenAccount(){
         <div className="container p-5 mb-2">
             <div className="row text-center">
                 <img className="mb-4" src="media/images/homeHero.png" alt="hero-image"/>
-                <h1 className="mt-4 mb-4">Open a Zerodha account</h1>
+                <h1 className="mt-4 mb-4">Open a FirstEarner account</h1>
                 <p className="mb-4 text-muted"> Modern platforms and apps, ₹0 investments, and flat ₹20 intraday and F&O trades.</p>
                 <Link to="/signup">
                     <button className="p-1 btn mb-3" style={{width : "16%",margin : "0 auto",backgroundColor : "#FFB200",color : "white",fontSize : "18px"}}>Sign up for free</button>
