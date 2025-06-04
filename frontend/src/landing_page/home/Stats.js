440bde8c86bc2a749fe5b93fffc8a88c2dadc13b
Pratham4403 <mohiteprathamesh2005@gmail.com>
2025-06-04 18:32:33 +0530

    Update Stats.js

--- frontend/src/landing_page/home/Stats.js
+++ frontend/src/landing_page/home/Stats.js
@@ -8,10 +8,10 @@ export default function Stats(){
                 <div className="col-6 p-5">
                     <h1 className="fs-2 mb-5">Trust with confidence</h1>
                     <h2 className="fs-5">Customer-first always</h2>
-                    <p className="text-muted">That's why 1.3+ crore customers trust Zerodha with ₹3.5+ lakh crores worth of equity investments.</p>
+                    <p className="text-muted">That's why 1.3+ crore customers trust FirstEarner with ₹3.5+ lakh crores worth of equity investments.</p>
                     <h2 className="fs-5">No spam or gimmicks</h2>
                     <p className="text-muted">No gimmicks, spam, "gamification", or annoying push notifications.High quality apps that you use at your pace, the way you like.</p>
-                    <h2 className="fs-5">The Zerodha universe</h2>
+                    <h2 className="fs-5">The FirstEarner universe</h2>
                     <p className="text-muted"> Not just an app, but a whole ecosystem. Our investments in 30+ fintech startups offer you tailored services specific to your needs.</p>
                     <h2 className="fs-5">Do better with money</h2>
                     <p className="text-muted">With initiatives like Nudge and Kill Switch, we don't just facilitate transactions, but actively help you do better with your money.</p>
