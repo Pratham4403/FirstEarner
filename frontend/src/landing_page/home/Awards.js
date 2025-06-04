efd33d0e638a13fe3a951d6e8196f8ecfd4d39f1
Pratham4403 <mohiteprathamesh2005@gmail.com>
2025-06-04 18:31:22 +0530

    Update Awards.js

--- frontend/src/landing_page/home/Awards.js
+++ frontend/src/landing_page/home/Awards.js
@@ -9,7 +9,7 @@ export default function Awards(){
                 </div>
                 <div className="col-6 p-3 mt-5">
                     <h1 className="mb-3">Largest stock broker in India</h1>
-                    <p className="mb-4">2+ million Zerodha clients contribute to over 15% of all retail order volumes in India daily by trading and investing in :</p>
+                    <p className="mb-4">2+ million FirstEarner clients contribute to over 15% of all retail order volumes in India daily by trading and investing in :</p>
                     <div className="row">
                         <div className="col-6">
                             <ul>
